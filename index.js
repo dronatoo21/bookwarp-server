5f49bec3aa446a99ceb104001cecf5a8898db6b7
dronatoo <mrdronatoo@gmail.com>
2024-01-26 04:12:15 -0800

    added get function all books with id at line:41

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function run() {
       res.send(result);
     });
 
+ 
     app.get('/allBooks/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
