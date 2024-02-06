3a4bdc63889d429dbd9ad9fb9c5a44752921520e
Shorif <shorifahamed571@gmail.com>
2024-02-07 00:30:13 +0600

    Book Delete Operation Created

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/allBooks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allBooksCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //
     //Blogs---------------------------
     //
